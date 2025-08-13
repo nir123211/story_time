31ae83d66d0aaa30070831cf15729ec84b4a7129
nir123211 <nir123211@gmail.com>
2025-08-13 14:35:47 +0300

    fix invalid music folder path

--- scripts/sounds_and_music/sounds_adder.py
+++ scripts/sounds_and_music/sounds_adder.py
@@ -6,6 +6,7 @@ from scripts.sounds_and_music.sound_api import add_sounds_mp3
 
 
 def add_music(story_dir):
+    music_folder = Path.cwd()/"scripts"/"sounds_and_music"/"music"
     print('Adding sounds:')
     lines = [line for line in os.listdir(story_dir) if (story_dir / line).is_dir() and 'line' in line]
     lines = sorted(lines, key=lambda folder: int(folder.replace('line', '')))
@@ -15,4 +16,4 @@ def add_music(story_dir):
         if (line_folder / 'music.txt').exists() and not (line_folder / 'music.mp3').exists():
             music_query = (line_folder / 'music.txt').read_text()
             print(f'Adding music: {music_query}')
-            shutil.copyfile(story_dir/".."/".."/"music"/music_query, line_folder / "music.mp3")
+            shutil.copyfile(music_folder/music_query, line_folder/"music.mp3")
